d39824c6b7f87200395466f598ec2d91705e63cb
maksym <maksym.iurchenko@gmail.com>
2020-08-23 10:18:29 -0700

    init commit

---
+++ src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+  sku: string;
+  name: string
+  description: string;
+  unitPrice: number;
+  imageUrl: string;
+  active: boolean;
+  unitsInStock: number;
+  datecreated: Date;
+  lastUpdated: Date;
+
+}
